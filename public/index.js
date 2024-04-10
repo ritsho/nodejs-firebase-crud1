6e13dc4723808b71a4177374ccfa87a253255d59
Rivky Shoham <142743496+ritsho@users.noreply.github.com>
2024-04-10 23:13:49 +0300

    login page working

--- public/index.js
+++ public/index.js
@@ -1,8 +1,9 @@
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js"; 
- // Import the functions you need from the SDKs you need
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,25 +16,27 @@ const firebaseConfig = {
   storageBucket: "crud1-3cbde.appspot.com",
   messagingSenderId: "256014504961",
   appId: "1:256014504961:web:8be89a24443195fbf1dc00",
-  measurementId: "G-0JPDNS976B"
+  measurementId: "G-0JPDNS976B",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-function login() {
+let btnLogin = document.getElementById("btnLogin");
+btnLogin.onclick = function () {
   // נשיג את שם המשתמש והסיסמה שהוא כתב
-  const username = document.getElementById("txtUsername").value;
+  const email = document.getElementById("txtEmail").value;
   const password = document.getElementById("txtPassword").value;
 
-  if (username == "" || password == "") {
+  if (email == "" || password == "") {
     alert("user name or password empty");
     return;
   }
 
   const auth = getAuth(app);
-  signInWithEmailAndPassword(auth, username, password)
+  signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
+      console.log("sign in succeess start");
       // צד השרת דיווח שהמשתמש כתב סיסמה נכונה אז נקבל את פרטי המשתמש שכתובים בפיירבייס
       const user = userCredential.user;
       console.log(user);
@@ -41,9 +44,10 @@ function login() {
       window.location.href = "users_page.html";
     })
     .catch((error) => {
+      console.log("sign in fail start");
       // המשתמש לא הצליח לבצע כניסה - נציג את השגיאה
       const errorCode = error.code;
       const errorMessage = error.message;
       alert(errorCode + "\n" + errorMessage);
     });
-}
+};
